82383ab26fe77cdf567a077ce3a1871a8d1f34c1
Forest Agostinelli <foresta@cse.sc.edu>
2024-03-11 10:14:15 -0400

    changes

--- train_test.sh
+++ train_test.sh
@@ -1,21 +1,26 @@
 ### Cube3
 ###### Train heuristic function
-python train.py --env cube3 --step_max 30 --batch_size 10000 --itrs_per_update 5000 --greedy_step_update_max 30 --max_itrs 2000000 --num_update_procs 48 --nnet_dir models/cube3/
+python run_train.py --env cube3 --step_max 30 --batch_size 10000 --itrs_per_update 5000 --greedy_step_update_max 30 --max_itrs 2000000 --num_update_procs 48 --nnet_dir models/cube3/
 
 ###### Search with trained heuristic function
-python search.py --states data/cube3/test/canon.pkl --heur models/cube3/current.pt --env cube3 --batch_size 10000 --weight 1.0 --results_dir results/cube3_cannon/ --redo --time_limit 200
-python search.py --states data/cube3/test/rand.pkl --heur models/cube3/current.pt --env cube3 --batch_size 10000 --weight 1.0 --results_dir results/cube3_rand/ --redo --time_limit 200
+python run_search.py --states data/cube3/test/canon.pkl --heur models/cube3/current.pt --env cube3 --batch_size 10000 --weight 1.0 --results_dir results/cube3_cannon/ --redo --time_limit 200
+python run_search.py --states data/cube3/test/rand.pkl --heur models/cube3/current.pt --env cube3 --batch_size 10000 --weight 1.0 --results_dir results/cube3_rand/ --redo --time_limit 200
 
 ### 15-puzzle
 ###### Train heuristic function
-python train.py --env puzzle15 --step_max 1000 --batch_size 10000 --itrs_per_update 5000 --greedy_step_update_max 100 --max_itrs 2000000 --num_update_procs 48 --nnet_dir models/puzzle15/
+python run_train.py --env puzzle15 --step_max 1000 --batch_size 10000 --itrs_per_update 5000 --greedy_step_update_max 100 --max_itrs 2000000 --num_update_procs 48 --nnet_dir models/puzzle15/
 
+###### Search with trained heuristic function
+python run_search.py --states data/puzzle15/test/canon.pkl --heur models/puzzle15/current.pt --env puzzle15 --batch_size 1000 --weight 1.0 --results_dir results/puzzle15_cannon/ --redo --time_limit 200
 
 ### 24-puzzle
 ###### Train heuristic function
-python train.py --env puzzle24 --step_max 1000 --batch_size 10000 --itrs_per_update 5000 --greedy_step_update_max 100 --max_itrs 4000000 --num_update_procs 48 --nnet_dir models/puzzle24/
+python run_train.py --env puzzle24 --step_max 1000 --batch_size 10000 --itrs_per_update 5000 --greedy_step_update_max 100 --max_itrs 4000000 --num_update_procs 48 --nnet_dir models/puzzle24/
 
 
 ### Sokoban
 ###### Train heuristic function
-python train.py --env sokoban --step_max 1000 --batch_size 1000 --itrs_per_update 5000 --greedy_step_update_max 30 --max_itrs 1000000 --num_update_procs 48 --nnet_dir models/sokoban/
+python run_train.py --env sokoban --step_max 1000 --batch_size 1000 --itrs_per_update 5000 --greedy_step_update_max 30 --max_itrs 1000000 --num_update_procs 48 --nnet_dir models/sokoban/
+
+###### Search with trained heuristic function
+python run_search.py --states data/sokoban/test/canon.pkl --heur models/sokoban/current.pt --env sokoban --batch_size 100 --weight 1.0 --results_dir results/sokoban/ --redo --time_limit 200
